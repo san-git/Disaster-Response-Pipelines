9ce11ce837bc6da39fb5ec4a17549a69ccbcedbf
san-git <sanchittiwari@hotmail.com>
2020-01-14 18:15:06 -0600

    Updated model training script
    Updated the model training script, not uploading the pickle file as it is big and taking too much time.

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,41 +1,112 @@
 import sys
+from sqlalchemy import create_engine
+
+import nltk
+nltk.download(['punkt', 'wordnet', 'stopwords'])
+
+import re
+import numpy as np
+import pandas as pd
+import pickle
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.svm import LinearSVC
+from sklearn.multiclass import OneVsRestClassifier
+
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
 
 
 def load_data(database_filepath):
-    pass
+
+    engine = create_engine('sqlite:///'+database_filepath)
+    df = pd.read_sql_table('DisasterResponse', con=engine)
+    X = df['message']
+    y = df.drop(['id','message', 'original', 'genre'], axis=1)
+    category_names = list(y.columns)
+    return X, y, category_names
 
 
 def tokenize(text):
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    detected_urls = re.findall(url_regex , text)
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
+        
+    # Convert text to lowercase and remove punctuation
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+    
+    # Tokenize words
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+    
+    # load stop words
+    stop_words = stopwords.words("english")
+    
+    # lemmatize andremove stop words
+    lemmatizer = WordNetLemmatizer()
+    tokens = [lemmatizer.lemmatize(word).strip() for word in tokens if word not in stop_words]
+
+    return tokens
     pass
 
 
 def build_model():
-    pass
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer = tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+        ])
+    return pipeline
+
+    parameters = {'vect__max_features': (None, 5000), 
+              'clf__estimator__n_estimators': [10, 20] 
+             } 
+    
+    model = GridSearchCV(pipeline, param_grid=parameters, scoring='f1_micro', verbose=1, n_jobs=-1)
+    
+    return model
 
 
-def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+
+
+def evaluate_model(model, X_test, y_test, category_names):
+    y_pred = model.predict(X_test)
+    class_report = classification_report(y_test, y_pred, target_names=category_names)
+    print(class_report)
+    
 
 
 def save_model(model, model_filepath):
-    pass
+    pickle.dump(model, open(model_filepath, 'wb'))
 
 
 def main():
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
-        X, Y, category_names = load_data(database_filepath)
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-        
+        X, y, category_names = load_data(database_filepath)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+   
         print('Building model...')
         model = build_model()
-        
+        print(y.shape)
+        print(X.shape)
         print('Training model...')
-        model.fit(X_train, Y_train)
+        model.fit(X_train, y_train)
         
         print('Evaluating model...')
-        evaluate_model(model, X_test, Y_test, category_names)
+        evaluate_model(model, X_test, y_test, category_names)
 
         print('Saving model...\n    MODEL: {}'.format(model_filepath))
         save_model(model, model_filepath)
